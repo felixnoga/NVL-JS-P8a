27dccfa7e95f0cbba06180d6af4cdb9cb22f320b
Felix Nogales <felixnoga@gmail.com>
2019-05-26 21:26:07 +0200

    Creacion del archivo javascript

---
+++ nvl-js-p8a.js
@@ -0,0 +1,9 @@
+var cadena = "Amigo";
+cadena = cadena.toLowerCase();
+
+
+cadena = "Amigo"; //Volvemos a poner la A en mayúsculas
+cadena = cadena.replace('A', 'a');
+
+cadena = "Amigo"; //Volvemos a poner la A en mayúsculas
+cadena = "a"+cadena.substr(1);
